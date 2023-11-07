7a1e7fa64cf6f7e3f8e6b2e2a572bee5f11c7438
MrBuhgatto <hbarcelo19496@iesjoanramis.org>
2023-11-07 13:47:20 +0100

    exercici 11 acabat

--- ex11.py
+++ ex11.py
@@ -4,13 +4,98 @@ def menu_principal():
           1. Calculadora de enters
           2. Calculadora de reals
           3. Sortir
-          """)
-    x = int(input("Elegeix una opció: "))
-    if x>0 and x<4:
-        return x
-    else:
-        return 0
+    
+    """)
+    a = int(input("Elegeix una opció: "))
+    return a
+def calculadora_enters():
+    print("Has triat la calculadora d'enters")
+    b = 1
+    while b>0:
+        print(""" 
+              1. Sumar 
+              2. Restar
+              3. Multiplicar 
+              4. Dividir
+              5. Salir
+        """)
+        b = int(input("Elegeix una opció: "))
+        match b:
+            case 1: #Sumar
+                x = int(input("Introdueix el primer nombre: "))
+                y = int(input("Elegeix el segon nombre: "))
+                print("{} + {} = {}".format(x, y, x+y))
+
+            case 2: #Restar
+                x = int(input("Introdueix el primer nombre: "))
+                y = int(input("Elegeix el segon nombre: "))
+                print("{} - {} = {}".format(x, y, x-y))
+            
+            case 3: #Multiplicar
+                x = int(input("Introdueix el primer nombre: "))
+                y = int(input("Elegeix el segon nombre: "))
+                print("{} * {} = {}".format(x, y, x*y))
+            
+            case 4: #Dividir
+                x = int(input("Introdueix el primer nombre: "))
+                y = int(input("Elegeix el segon nombre: "))
+                print("{} / {} = {}".format(x, y, x/y))
+
+            case 5: #Sortir
+                print("Adeu :,(")
+                b = -1
+       
+
+
+def calculadora_reals():
+    print("Has triat la calculadora de reals: ")
+    c = 1
+    while c>0:
+        print(""" 
+              1. Sumar 
+              2. Restar
+              3. Multiplicar 
+              4. Dividir
+              5. Salir
+        """)
+        c = int(input("Elegeix una opció: "))
+        match c:
+            case 1: #Sumar
+                x = float(input("Introdueix el primer nombre: "))
+                y = float(input("Elegeix el segon nombre: "))
+                print("{} + {} = {}".format(x, y, x+y))
+
+            case 2: #Restar
+                x = float(input("Introdueix el primer nombre: "))
+                y = float(input("Elegeix el segon nombre: "))
+                print("{} - {} = {}".format(x, y, x-y))
+            
+            case 3: #Multiplicar
+                x = float(input("Introdueix el primer nombre: "))
+                y = float(input("Elegeix el segon nombre: "))
+                print("{} * {} = {}".format(x, y, x*y))
+            
+            case 4: #Dividir
+                x = float(input("Introdueix el primer nombre: "))
+                y = float(input("Elegeix el segon nombre: "))
+                print("{} / {} = {}".format(x, y, x/y))
+            
+            case 5: #Sortir
+                print("Adeu :,(")
+                c = -1
+       
         
 #Programa principal
-a = menu_principal
-print(a)
+
+a = 1
+while a>0:
+    a=menu_principal()
+    match a:
+        case 1:
+            calculadora_enters()
+        case 2:
+            calculadora_reals()
+        case 3:
+            a = -1
+        case other:
+            print("Opció no vàlida")
